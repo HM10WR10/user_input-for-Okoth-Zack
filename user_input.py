6c80db8e1a34c3bfbb5ceed74691efc86f01b66b
HM10WR10 <146798888+HM10WR10@users.noreply.github.com>
2024-03-28 21:40:59 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+"""PYTHON CODE THAT ASKS THE USER TO ENTER THEIR NAME LOCATION AND AGE AND THEN OUTPUTS THE SAME AS A SINGLE SENTENCE"""
+name=input('what is your name?') #getting the name of the user and storing it in the variable 'name'
+age=input('what is your age?')  #getting the age of the user and storing it in the variable 'age'
+location=input('what is your location?') #getting t6he location of the user and storing it in the variable 'location'
+print('Hello',name+',you are',age+' years old and you live in',location) #printing all collected information as a single sentence 
